5b8a6a39269ccaf817f1d1183cb91e2df9f86709
camsh69 <camsh69@gmail.com>
2022-02-18 19:32:20 +0000

    adding GuitarString passing tests

--- music_shop/src/main/java/accessories/GuitarStrings.java
+++ music_shop/src/main/java/accessories/GuitarStrings.java
@@ -1,4 +1,22 @@
 package accessories;
 
+import shop.Cost;
+
 public class GuitarStrings {
+
+    private int gauge;
+    private Cost pricing;
+
+    public GuitarStrings(int gauge, Cost pricing) {
+        this.gauge = gauge;
+        this.pricing = pricing;
+    }
+
+    public int getGauge() {
+        return gauge;
+    }
+
+    public Cost getPricing() {
+        return pricing;
+    }
 }
--- music_shop/src/test/java/GuitarStringsTest.java
+++ music_shop/src/test/java/GuitarStringsTest.java
@@ -1,2 +1,46 @@
+import accessories.GuitarStrings;
+import org.junit.Before;
+import org.junit.Test;
+import shop.Cost;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuitarStringsTest {
+
+    GuitarStrings guitarStrings;
+    Cost cost;
+
+    @Before
+    public void before() {
+        cost = new Cost(5.00, 6.00);
+        guitarStrings = new GuitarStrings(10, cost);
+    }
+
+    @Test
+    public void hasGauge() {
+        assertEquals(10, guitarStrings.getGauge());
+    }
+
+    @Test
+    public void hasBuyingPrice() {
+        assertEquals(5.00, guitarStrings.getPricing().getBuyingPrice(), 0.0);
+    }
+
+    @Test
+    public void canSetBuyingPrice() {
+        guitarStrings.getPricing().setBuyingPrice(9.00);
+        assertEquals(9.00, guitarStrings.getPricing().getBuyingPrice(), 0.0);
+    }
+
+    @Test
+    public void hasSellingPrice() {
+        assertEquals(6.00, guitarStrings.getPricing().getSellingPrice(), 0.0);
+    }
+
+    @Test
+    public void canSetSellingPrice() {
+        guitarStrings.getPricing().setSellingPrice(12.00);
+        assertEquals(12.00, guitarStrings.getPricing().getSellingPrice(), 0.0);
+    }
 }
+
