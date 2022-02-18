6308884ed71aeca6a3675ac0c56a3cce00edb8fb
camsh69 <camsh69@gmail.com>
2022-02-18 17:36:05 +0000

    adding passing Drumsticks tests

--- music_shop/src/main/java/accessories/DrumSticks.java
+++ music_shop/src/main/java/accessories/DrumSticks.java
@@ -1,4 +1,22 @@
 package accessories;
 
+import shop.Cost;
+
 public class DrumSticks {
+
+    private int thickness;
+    private Cost pricing;
+
+    public DrumSticks(int thickness, Cost pricing) {
+        this.thickness = thickness;
+        this.pricing = pricing;
+    }
+
+    public int getThickness() {
+        return thickness;
+    }
+
+    public Cost getPricing() {
+        return pricing;
+    }
 }
--- music_shop/src/test/java/DrumSticksTest.java
+++ music_shop/src/test/java/DrumSticksTest.java
@@ -1,2 +1,34 @@
+import accessories.DrumSticks;
+import org.junit.Before;
+import org.junit.Test;
+import shop.Cost;
+
+import static org.junit.Assert.assertEquals;
+
 public class DrumSticksTest {
+
+    DrumSticks drumSticks;
+    Cost cost;
+
+    @Before
+    public void before() {
+        cost = new Cost(10.00, 15.00);
+        drumSticks = new DrumSticks(5, cost);
+    }
+
+    @Test
+    public void hasThickness() {
+        assertEquals(5, drumSticks.getThickness(), 0.0);
+    }
+
+    @Test
+    public void hasBuyingPrice() {
+        assertEquals(10.00, drumSticks.getPricing().getBuyingPrice(), 0.0);
+    }
+
+    @Test
+    public void canSetBuyingPrice() {
+        drumSticks.getPricing().setBuyingPrice(9.00);
+        assertEquals(9.00, drumSticks.getPricing().getBuyingPrice(), 0.0);
+    }
 }
