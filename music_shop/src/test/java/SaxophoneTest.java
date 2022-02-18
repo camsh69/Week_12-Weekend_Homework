bee7cd7b8383dee1766f9b90230a3f13a5301a29
camsh69 <camsh69@gmail.com>
2022-02-18 17:18:38 +0000

    Adding passing Saxophone tests

--- music_shop/src/test/java/SaxophoneTest.java
+++ music_shop/src/test/java/SaxophoneTest.java
@@ -1,2 +1,29 @@
+import instruments.InstrumentType;
+import instruments.Saxophone;
+import org.junit.Before;
+import org.junit.Test;
+import shop.Cost;
+
+import static org.junit.Assert.assertEquals;
+
 public class SaxophoneTest {
+
+    Saxophone saxophone;
+    Cost cost;
+
+    @Before
+    public void before() {
+        cost = new Cost(250.00, 299.99);
+        saxophone = new Saxophone("Brass", "Brass", InstrumentType.BRASS, "Eastar", "123A", cost, "Alto", "Eb");
+    }
+
+    @Test
+    public void hasSaxoPhoneType() {
+        assertEquals("Alto", saxophone.getSaxophoneType());
+    }
+
+    @Test
+    public void hasKey() {
+        assertEquals("Eb", saxophone.getKey());
+    }
 }
