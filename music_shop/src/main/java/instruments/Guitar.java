0a7e009acbcdc31e6b1c4b269cde7f04d986faf1
camsh69 <camsh69@gmail.com>
2022-02-18 14:58:22 +0000

    added first Guiter inherited test

--- music_shop/src/main/java/instruments/Guitar.java
+++ music_shop/src/main/java/instruments/Guitar.java
@@ -1,4 +1,9 @@
 package instruments;
 
-public class Guitar {
+import shop.Cost;
+
+public class Guitar extends Instrument {
+    public Guitar(String material, String colour, InstrumentType type, String manufacturer, String model, Cost pricing) {
+        super(material, colour, type, manufacturer, model, pricing);
+    }
 }
--- music_shop/src/test/java/GuitarTest.java
+++ music_shop/src/test/java/GuitarTest.java
@@ -1,2 +1,24 @@
+import instruments.Guitar;
+import instruments.InstrumentType;
+import org.junit.Before;
+import org.junit.Test;
+import shop.Cost;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuitarTest {
+
+    Guitar guitar;
+    Cost cost;
+
+    @Before
+    public void before() {
+        cost = new Cost(250.00, 300.00);
+        guitar = new Guitar("Alder", "Black", InstrumentType.STRING, "Fender", "Telecaster", cost);
+    }
+
+    @Test
+    public void hasMaterial() {
+        assertEquals("Alder", guitar.getMaterial());
+    }
 }
